c8036cdedfbd194c033e45337a2abe6bae4785b6
marienannie-itson <maria.soto267714@potros.itson.edu.mx>
2025-09-01 10:00:31 -0700

    Clase Cilindro Terminada

--- Asignacion01/Cilindro/src/cilindro/Cilindro.java
+++ Asignacion01/Cilindro/src/cilindro/Cilindro.java
@@ -1,7 +1,19 @@
 package cilindro;
 
 public class Cilindro {
-    public static void main(String[] args) {
-        // hello Mundo
-    }
+     private double radio;
+     private double altura;
+     
+     public Cilindro(double radio, double altura) {
+         this.radio = radio;
+         this.altura = altura;
+     }
+     
+     public double area() {
+        return (Math.PI * radio * radio * 2) + (Math.PI * 2 * radio * altura);
+     }
+     
+     public double volumen (){
+         return Math.PI * radio * radio * altura;
+     }
 }
