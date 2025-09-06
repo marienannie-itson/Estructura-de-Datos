5af383dd4a5fbdad499d69846a9d2871f7676969
marienannie-itson <maria.soto267714@potros.itson.edu.mx>
2025-09-06 11:49:22 -0700

    Add CilindroImp implementation class

--- Asignacion02/src/main/java/implementaciones/CilindroImp.java
+++ Asignacion02/src/main/java/implementaciones/CilindroImp.java
@@ -1,4 +1,23 @@
 package implementaciones;
 
-public class CilindroImp {
+import TDA.Cilindro;
+
+public class CilindroImp extends Cilindro {
+
+    public CilindroImp(double radio, double altura) {
+        super(radio, altura);
+
+        if (radio < 0 || altura < 0) {
+            throw new IllegalArgumentException("La altura y el radio no pueden ser negativos");
+        }
+    }
+
+    @Override
+    public double area() { return 2 * Math.PI * radio * (altura + radio);}
+
+    @Override
+    public double volumen() { return Math.PI * radio * radio * altura; }
+
+    @Override
+    public String toString(){ return "Cilindro(" + radio + ", " + altura + ")"; }
 }
