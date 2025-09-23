2f9191715abb540b3343294ddafac2bc031d2d19
marienannie-itson <maria.soto267714@potros.itson.edu.mx>
2025-09-23 09:43:30 -0700

    Crear proyecto en Java Maven

---
+++ Asignacion04/src/main/java/c1147c/asignacion04/Asignacion04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package c1147c.asignacion04;
+
+/**
+ *
+ * @author maria
+ */
+public class Asignacion04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
